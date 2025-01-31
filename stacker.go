835e9a2856cf56091a02bc688d5976d293bc6ca3
Ewald Horn <ewaldhorn@gmail.com>
2025-01-31 09:39:08 +0200

    Added some more smoke tests

--- stacker.go
+++ stacker.go
@@ -17,38 +17,51 @@ func NewStacker[Type any]() *Stacker[Type] {
 }
 
 // ----------------------------------------------------------------------------
-func (stack *Stacker[Type]) Push(element Type) {
-	stack.mutex.Lock()
-	defer stack.mutex.Unlock()
-	stack.Data = append(stack.Data, element)
+// Returns the number of elements in the stack
+func (stacker *Stacker[Type]) Length() int {
+	stacker.mutex.Lock()
+	defer stacker.mutex.Unlock()
+	length := len(stacker.Data)
+
+	return length
 }
 
 // ----------------------------------------------------------------------------
-func (stack *Stacker[Type]) Pop() (Type, error) {
-	stack.mutex.Lock()
-	defer stack.mutex.Unlock()
-	l := len(stack.Data)
+// Pushed a new item into the stack
+func (stacker *Stacker[Type]) Push(element Type) {
+	stacker.mutex.Lock()
+	defer stacker.mutex.Unlock()
+	stacker.Data = append(stacker.Data, element)
+}
 
-	if l == 0 {
+// ----------------------------------------------------------------------------
+// Pops and returns the first item on the stack, possibly error if the stack is empty
+func (stacker *Stacker[Type]) Pop() (Type, error) {
+	stacker.mutex.Lock()
+	defer stacker.mutex.Unlock()
+	length := len(stacker.Data)
+
+	if length == 0 {
 		var empty Type
 		return empty, errors.New("empty Stack")
 	}
-	element := stack.Data[l-1]
-	stack.Data = stack.Data[:l-1]
+	element := stacker.Data[length-1]
+	stacker.Data = stacker.Data[:length-1]
 
 	return element, nil
 }
 
 // ----------------------------------------------------------------------------
-func (stack *Stacker[Type]) Peek() (Type, error) {
-	stack.mutex.Lock()
-	defer stack.mutex.Unlock()
-	l := len(stack.Data)
+// Returns the first entry in the stack, does not remove it, can error on empty
+func (stacker *Stacker[Type]) Peek() (Type, error) {
+	stacker.mutex.Lock()
+	defer stacker.mutex.Unlock()
+	length := len(stacker.Data)
 
-	if l == 0 {
+	if length == 0 {
 		var empty Type
 		return empty, errors.New("empty Stack")
 	}
 
-	return stack.Data[l-1], nil
+	return stacker.Data[length-1], nil
 }
--- stacker_test.go
+++ stacker_test.go
@@ -24,6 +24,30 @@ func Test_Stacker_Empty(t *testing.T) {
 
 // ----------------------------------------------------------------------------
 func Test_Stacker_Smoke(t *testing.T) {
+	myStack := NewStacker[int]()
+
+	if myStack.Length() != 0 {
+		t.Errorf("[smoke] Expected a length of 0, got %d", myStack.Length())
+	}
+
+	// add 1..5
+	for i := range 5 {
+		myStack.Push(i + 1)
+	}
+
+	result, err := myStack.Peek()
+
+	if err != nil {
+		t.Errorf("[smoke] didn't expect '%s' I tell you!", err.Error())
+	}
+
+	if result != 5 {
+		t.Errorf("[smoke] I expected 5, got %d instead", result)
+	}
+
+	if myStack.Length() != 5 {
+		t.Errorf("[smoke] Expected a length of 5, got %d", myStack.Length())
+	}
 
 }
 
