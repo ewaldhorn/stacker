4561d29e7a559d8040c9f57c9ef0298dd232cfd8
Ewald Horn <ewaldhorn@gmail.com>
2025-01-31 09:48:05 +0200

    More tests

--- stacker_test.go
+++ stacker_test.go
@@ -1,6 +1,9 @@
 package stacker
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // ----------------------------------------------------------------------------
 func Test_Stacker_Empty(t *testing.T) {
@@ -35,6 +38,7 @@ func Test_Stacker_Smoke(t *testing.T) {
 		myStack.Push(i + 1)
 	}
 
+	// just peek for now
 	result, err := myStack.Peek()
 
 	if err != nil {
@@ -49,9 +53,77 @@ func Test_Stacker_Smoke(t *testing.T) {
 		t.Errorf("[smoke] Expected a length of 5, got %d", myStack.Length())
 	}
 
+	// now pop one
+	result, err = myStack.Pop()
+
+	if err != nil {
+		t.Errorf("[smoke] didn't expect '%s' I tell you!", err.Error())
+	}
+
+	if result != 5 {
+		t.Errorf("[smoke] I expected 5, got %d instead", result)
+	}
+
+	if myStack.Length() != 4 {
+		t.Errorf("[smoke] Expected a length of 4, got %d", myStack.Length())
+	}
+
 }
 
 // ----------------------------------------------------------------------------
 func Test_Stacker_Large(t *testing.T) {
+	myStack := NewStacker[string]()
 
+	if myStack.Length() != 0 {
+		t.Errorf("[large] Expected a length of 0, got %d", myStack.Length())
+	}
+
+	// add 1..1000
+	for i := range 1000 {
+		myStack.Push(fmt.Sprintf("Sentence %d", i+1))
+	}
+
+	if myStack.Length() != 1000 {
+		t.Errorf("[large] Expected a length of 1000, got %d", myStack.Length())
+	}
+
+	// now remove 100 items
+	for range 100 {
+		_, _ = myStack.Pop()
+	}
+
+	if myStack.Length() != 900 {
+		t.Errorf("[large] Expected a length of 900, got %d", myStack.Length())
+	}
+
+	msg, err := myStack.Peek()
+
+	if err != nil {
+		t.Errorf("[large] failed with '%s', expected no error", err.Error())
+	}
+
+	if msg != "Sentence 900" {
+		t.Errorf("[large] Wanted 'Sentence 900', got '%s'", msg)
+	}
+
+	// do a number of peeks
+	for range 10000 {
+		_, _ = myStack.Peek()
+	}
+
+	if myStack.Length() != 900 {
+		t.Errorf("[large] Expected a length of 900, got %d (peeks)", myStack.Length())
+	}
+
+	for myStack.Length() > 0 {
+		_, err = myStack.Pop()
+
+		if err != nil {
+			t.Errorf("[large] error popping all '%s'", err.Error())
+		}
+	}
+
+	if myStack.Length() != 0 {
+		t.Errorf("[large] stack should be empty now, it contains %d elements", myStack.Length())
+	}
 }
